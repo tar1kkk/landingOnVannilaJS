5bd5ad8b71cec7988244373bd1f4b2e4938fddf8
Taras <81424893+tar1kkk@users.noreply.github.com>
2023-01-03 15:11:10 +0200

    fix

--- webpack.config.js
+++ webpack.config.js
@@ -8,7 +8,7 @@ module.exports = {
 	output: {
 		clean: true,
 		filename: "[name].bundle.js",
-		path: path.resolve(__dirname, "dist"),
+		path: path.resolve(__dirname, "./src"),
 	},
 	mode: "development",
 	devServer: {
